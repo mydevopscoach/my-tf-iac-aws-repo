6549cc8528af3d993d61f26a538b931119a26fad
mydevopscoach <30705206+mydevopscoach@users.noreply.github.com>
2021-07-01 15:33:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-
 #Create security group with firewall rules
 resource "aws_security_group" "my_security_group" {
   name        = var.security_group
@@ -35,6 +34,7 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
+# Create AWS ec2 instance
 resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   key_name = var.key_name
