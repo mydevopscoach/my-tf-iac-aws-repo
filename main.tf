9a78b5e5589ca76f852dd9387e5e53929c347b11
root <root@ip-172-31-17-126.ec2.internal>
2023-01-20 07:22:41 +0000

    added ec2

--- main.tf
+++ main.tf
@@ -16,4 +16,10 @@ provider "aws" {
 resource "aws_vpc" "exa222le" {
   cidr_block = "10.0.0.0/16"
 }
+
+# Create an EC2 instance
+resource "aws_instance" "RHEL by terraform" {
+  ami           = "ami-08e637cea2f053dfa"
+  instance_type = "t2.micro"
+}
 # this is modified for pull request, again secound .
