57bfeb9df5e2df91d9030956859be41395c16507
root <root@ip-172-31-17-126.ec2.internal>
2023-01-24 06:39:12 +0000

    replaced ec2

--- main.tf
+++ main.tf
@@ -12,13 +12,11 @@ provider "aws" {
   region     = "us-east-1"
   shared_credentials_file = "/root/samba/.aws/credentials"
 }
-# Create a VPC
-resource "aws_vpc" "exa222le" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Create an EC2 instance
-resource "aws_instance" "RHEL by terraform" {
+#EC2 Instance
+resource "aws_instance" "my-ec2-instance" {
   ami           = "ami-08e637cea2f053dfa"
   instance_type = "t2.micro"
+  tags = {
+    Name = "terraform_example"
+  }
 }
