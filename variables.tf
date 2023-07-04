a9f0e9ae093565292851220f3e5664904161fad4
AlexAdetoye <109541219+AlexAdetoye@users.noreply.github.com>
2023-07-04 14:16:13 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  default     =  "production-xela"
 }
 
 variable "instance_type" {
@@ -25,5 +25,5 @@ variable "tag_name" {
 
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0b9064170e32bde34"
+  default     = "ami-02d8bad0a1da4b6fd"
 }
